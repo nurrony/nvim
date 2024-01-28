26b6e15df86b160d69f7ddfcb5719947f8a4e9f7
Nur Rony <pro.nmrony@gmail.com>
2024-01-28 15:40:52 +0600

    update which-key setup

--- lua/nurrony/plugins/which-key.lua
+++ lua/nurrony/plugins/which-key.lua
@@ -1,3 +1,5 @@
+-- which-key helps you remember key bindings by showing a popup
+-- with the active keybindings of the command you started typing.
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
@@ -6,8 +8,29 @@ return {
     vim.o.timeoutlen = 500
   end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    plugins = { spelling = true },
+    defaults = {
+      mode = { "n", "v" },
+      ["g"] = { name = "+goto" },
+      ["gs"] = { name = "+surround" },
+      ["]"] = { name = "+next" },
+      ["["] = { name = "+prev" },
+      ["<leader><tab>"] = { name = "+tabs" },
+      ["<leader>b"] = { name = "+buffer" },
+      ["<leader>c"] = { name = "+code" },
+      ["<leader>f"] = { name = "+file/find" },
+      ["<leader>g"] = { name = "+git" },
+      ["<leader>gh"] = { name = "+hunks" },
+      ["<leader>q"] = { name = "+quit/session" },
+      ["<leader>s"] = { name = "+search" },
+      ["<leader>u"] = { name = "+ui" },
+      ["<leader>w"] = { name = "+windows" },
+      ["<leader>x"] = { name = "+diagnostics/quickfix" },
+    },
   },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    wk.register(opts.defaults)
+  end,
 }
