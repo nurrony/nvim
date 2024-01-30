8fdc070bbd8119096cc6a8fb49806c1433c60965
Nur Rony <pro.nmrony@gmail.com>
2024-01-30 19:04:30 +0600

    update neo tree config

--- lua/nurrony/plugins/neo-tree.lua
+++ lua/nurrony/plugins/neo-tree.lua
@@ -1,35 +1,180 @@
+-- return {
+--   'nvim-neo-tree/neo-tree.nvim',
+--   branch = 'v3.x',
+--   keys = {
+--     { '<C-n>', '<CMD>Neotree filesystem toggle right<CR>', desc = 'Toggle file explorer' },
+--   },
+--   dependencies = {
+--     'nvim-lua/plenary.nvim',
+--     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+--     'MunifTanjim/nui.nvim',
+--   },
+--   config = function()
+--     vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
+--     vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
+--     vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
+--     vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
+--     local neoTree = require('neo-tree')
+--     neoTree.setup({
+--       close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+--       filesystem = {
+--         follow_current_file = {
+--           enabled =true,
+--         },
+--         filtered_items = {
+--           visible = true,
+--           hide_dotfiles = false,
+--           hide_hidden = false,
+--         },
+--       },
+--     })
+
+--     -- keymaps
+--     vim.keymap.set('n', '<C-n>', '<cmd>Neotree filesystem toggle right<cr>', {noremap = true, silent = true, desc = 'Toggle file explorer' }) -- toggle file explorer
+--   end,
+-- }
+
+
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  branch = 'v3.x',
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  cmd = "Neotree",
   keys = {
-    { '<C-n>', '<CMD>Neotree filesystem toggle right<CR>', desc = 'Toggle file explorer' },
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, position = 'right' })
+      end,
+      desc = "Explorer NeoTree",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree", remap = true },
+
+    {
+      "<leader>ge",
+      function()
+        require("neo-tree.command").execute({ source = "git_status", position = 'right', toggle = true })
+      end,
+      desc = "Git explorer",
+    },
+    {
+      "<leader>be",
+      function()
+        require("neo-tree.command").execute({ source = "buffers", position = 'right', toggle = true })
+      end,
+      desc = "Buffer explorer",
+    },
   },
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
+  deactivate = function()
+    vim.cmd([[Neotree close]])
+  end,
+  init = function()
+    if vim.fn.argc(-1) == 1 then
+      local stat = vim.loop.fs_stat(vim.fn.argv(0))
+      if stat and stat.type == "directory" then
+        require("neo-tree")
+      end
+    end
+  end,
+  opts = {
+    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+    close_if_last_window = true,
+    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+    filesystem = {
+      bind_to_cwd = true,
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_hidden = false,
+      },
+      follow_current_file = {
+        enabled = true,
+      },
+      use_libuv_file_watcher = true,
+    },
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+        ["Y"] = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          vim.fn.setreg("+", path, "c")
+        end,
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+    },
   },
-  config = function()
+  config = function(_, opts)
+
     vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
     vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
     vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
     vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
-    local neoTree = require('neo-tree')
-    neoTree.setup({
-      close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
-      filesystem = {
-        follow_current_file = {
-          enabled =true,
-        },
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_hidden = false,
-        },
-      },
-    })
 
-    -- keymaps
-    vim.keymap.set('n', '<C-n>', '<cmd>Neotree filesystem toggle right<cr>', {noremap = true, silent = true, desc = 'Toggle file explorer' }) -- toggle file explorer
+    local function get_clients(opts)
+      local ret = {}
+      if vim.lsp.get_clients then
+        ret = vim.lsp.get_clients(opts)
+      else
+        ret = vim.lsp.get_active_clients(opts)
+        if opts and opts.method then
+          ret = vim.tbl_filter(function(client)
+            return client.supports_method(opts.method, { bufnr = opts.bufnr })
+          end, ret)
+        end
+      end
+      return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
+    end
+
+
+    local function on_attach(on_attach)
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local buffer = args.buf ---@type number
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          on_attach(client, buffer)
+        end,
+      })
+    end
+
+    --- @param from string
+    --- @param to string
+    local function on_rename(from, to)
+      local clients = get_clients()
+      for _, client in ipairs(clients) do
+        if client.supports_method("workspace/willRenameFiles") then
+          ---@diagnostic disable-next-line: invisible
+          local resp = client.request_sync("workspace/willRenameFiles", {
+            files = {
+              {
+                oldUri = vim.uri_from_fname(from),
+                newUri = vim.uri_from_fname(to),
+              },
+            },
+          }, 1000, 0)
+          if resp and resp.result ~= nil then
+            vim.lsp.util.apply_workspace_edit(resp.result, client.offset_encoding)
+          end
+        end
+      end
+    end
+
+    local function on_move(data)
+     on_rename(data.source, data.destination)
+    end
+
+    local events = require("neo-tree.events")
+    opts.event_handlers = opts.event_handlers or {}
+    vim.list_extend(opts.event_handlers, {
+      { event = events.FILE_MOVED, handler = on_move },
+      { event = events.FILE_RENAMED, handler = on_move },
+    })
+    require("neo-tree").setup(opts)
   end,
 }
