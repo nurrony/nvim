d6256f0430f1fdf69fa32628cf2bf7c10340d9b0
Nur Rony <pro.nmrony@gmail.com>
2024-02-02 22:03:17 +0600

    add lsp util

--- lua/nurrony/utils/lsp.lua
+++ lua/nurrony/utils/lsp.lua
@@ -46,7 +46,20 @@ local function on_rename(from, to)
   end
 end
 
+function inlay_hints(buf, value)
+  local ih = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
+  if type(ih) == "function" then
+    ih(buf, value)
+  elseif type(ih) == "table" and ih.enable then
+    if value == nil then
+      value = not ih.is_enabled(buf)
+    end
+    ih.enable(buf, value)
+  end
+end
+
 return {
   on_attach = on_attach,
   on_rename = on_rename,
+  inlay_hints = inlay_hints,
 }
