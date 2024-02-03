19f5286245bf834535874e6f4d0209b41c000344
Nur Rony <pro.nmrony@gmail.com>
2024-02-04 02:36:33 +0600

    add bufferline and noise

--- lua/nurrony/plugins/noise.lua
+++ lua/nurrony/plugins/noise.lua
@@ -23,7 +23,15 @@ return {
   opts = {
     lsp = {
       progress = {
-        enabled = false,
+        enabled = true,
+        -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+        -- See the section on formatting for more details on how to customize.
+        --- @type NoiceFormat|string
+        format = "lsp_progress",
+        --- @type NoiceFormat|string
+        format_done = "lsp_progress_done",
+        throttle = 1000 / 30, -- frequency to update lsp progress message
+        view = "mini",
       },
       override = {
         -- override the default lsp markdown formatter with Noice
