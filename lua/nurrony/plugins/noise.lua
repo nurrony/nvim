a9d113455b0f6902a8ea69d200920318673c7ab2
Nur Rony <pro.nmrony@gmail.com>
2024-02-04 02:23:43 +0600

    update and add noise plugin

---
+++ lua/nurrony/plugins/noise.lua
@@ -0,0 +1,60 @@
+
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    'stevearc/dressing.nvim',
+    "MunifTanjim/nui.nvim",  -- better vim.ui
+    {
+      "rcarriga/nvim-notify",
+      lazy = true,
+      keys = {
+        {
+          "<leader>un",
+          function()
+              require("notify").dismiss({ silent = true, pending = true })
+          end,
+          desc = "Clear notifications",
+        },
+      },
+      opts = { timeout = 3000 },
+    },
+  },
+  opts = {
+    lsp = {
+      progress = {
+        enabled = false,
+      },
+      override = {
+        -- override the default lsp markdown formatter with Noice
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        -- override the lsp markdown formatter with Noice
+        ["vim.lsp.util.stylize_markdown"] = true,
+        -- override cmp documentation with Noice (needs the other options to work)
+        ["cmp.entry.get_documentation"] = true,
+      },
+      --NOTE: install parsers for markdown and markdown_inline to see markdown documentation
+      hover = {
+        enabled = true,
+      },
+      signature = {
+        enabled = false,
+      },
+      message = {
+        enabled = true, -- Messages shown by lsp servers
+      },
+    },
+    health = {
+      checker = false, -- Disable if you don't want health checks to run
+    },
+    presets = {
+      -- you can enable a preset by setting it to true, or a table that will override the preset config
+      -- you can also add custom presets that you can enable/disable with enabled=true
+      bottom_search = false, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = false, -- long messages will be sent to a split
+      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+  },
+}
