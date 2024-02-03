e3e93c5a93744aff50f4237df5795c4e53a9119a
Nur Rony <pro.nmrony@gmail.com>
2024-02-04 01:58:59 +0600

    add indent blankline

---
+++ lua/nurrony/plugins/indent-blankline.lua
@@ -0,0 +1,11 @@
+-- indent guides for Neovim
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    main = "ibl",
+    opts = {
+        indent = {
+            char = "▏",
+        },
+    },
+}
