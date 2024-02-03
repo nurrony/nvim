a537c715865299f5a2649e8d519098c91247b473
Nur Rony <pro.nmrony@gmail.com>
2024-02-04 02:19:57 +0600

    add min indentscope plugin

---
+++ lua/nurrony/plugins/mini.indentscope.lua
@@ -0,0 +1,29 @@
+-- active indent guide and indent text objects
+return {
+  "echasnovski/mini.indentscope",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+  },
+  init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+          pattern = {
+              "help",
+              "alpha",
+              "dashboard",
+              "neo-tree",
+              "Trouble",
+              "lazy",
+              "mason",
+              "notify",
+              "toggleterm",
+              "lazyterm",
+          },
+          callback = function()
+              vim.b.miniindentscope_disable = true
+          end,
+      })
+  end,
+}
