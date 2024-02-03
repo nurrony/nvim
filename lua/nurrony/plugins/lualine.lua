4e3a822a765304542bd97be1251fe1dc9fa9c4ec
Nur Rony <pro.nmrony@gmail.com>
2024-02-04 02:33:10 +0600

    add lualine

---
+++ lua/nurrony/plugins/lualine.lua
@@ -0,0 +1,36 @@
+-- statusline
+return {
+  "nvim-lualine/lualine.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = function()
+    local function total_num_lines()
+      return vim.api.nvim_buf_line_count(0)
+    end
+
+    return {
+      options = {
+        theme = "catppuccin", -- "auto", "catppuccin",
+        component_separators = "|",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+          lualine_a = {
+            { "mode", separator = { left = "" }, right_padding = 2 },
+          },
+          lualine_b = { "branch", "diff" },
+          lualine_c = {
+            {
+              "filename",
+              file_status = true, -- displays file status (readonly status, modified status)
+              path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+            },
+          },
+          lualine_x = { "encoding", "fileformat" },
+          lualine_y = { "filetype", total_num_lines },
+          lualine_z = {
+            { "location", separator = { right = "" }, left_padding = 2 },
+          },
+      },
+    }
+  end,
+}
