7575ca648d3d79560ce04fa36463ba2174aff447
Nur Rony <pro.nmrony@gmail.com>
2024-02-07 02:03:45 +0600

    format defaults config

--- lua/nurrony/plugins/whichkey.lua
+++ lua/nurrony/plugins/whichkey.lua
@@ -4,58 +4,58 @@ return {
   event = "VeryLazy",
   opts = {
       plugins = {
-          marks = true, -- shows a list of your marks on ' and `
-          registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-          -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-          -- No actual key bindings are created
-          spelling = {
-              enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-              suggestions = 20, -- how many suggestions should be shown in the list?
-          },
-          presets = {
-              operators = true, -- adds help for operators like d, y, ...
-              motions = true, -- adds help for motions
-              text_objects = true, -- help for text objects triggered after entering an operator
-              windows = true, -- default bindings on <c-w>
-              nav = true, -- misc bindings to work with windows
-              z = true, -- bindings for folds, spelling and others prefixed with z
-              g = true, -- bindings for prefixed with g
-          },
+        marks = true, -- shows a list of your marks on ' and `
+        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+        -- No actual key bindings are created
+        spelling = {
+          enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+          suggestions = 20, -- how many suggestions should be shown in the list?
+        },
+        presets = {
+          operators = true, -- adds help for operators like d, y, ...
+          motions = true, -- adds help for motions
+          text_objects = true, -- help for text objects triggered after entering an operator
+          windows = true, -- default bindings on <c-w>
+          nav = true, -- misc bindings to work with windows
+          z = true, -- bindings for folds, spelling and others prefixed with z
+          g = true, -- bindings for prefixed with g
+        },
       },
       -- add operators that will trigger motion and text object completion
       -- to enable all native operators, set the preset / operators plugin above
       operators = { gc = "Comments" },
       motions = {
-          count = true,
+        count = true,
       },
   },
   config = function(_, opts)
-      local wk = require("which-key")
+    local whichkey = require("which-key")
 
-      wk.setup(opts)
-      local keymaps = {
-          mode               = { "n", "v" },
-          ["<leader>g"]      = { name = "+git" },
-          ["g"]              = { name = "+goto" },
-          ["]"]              = { name = "+next" },
-          ["["]              = { name = "+prev" },
-          ["<leader><tab>"]  = { name = "+tabs" },
-          ["<leader>c"]      = { name = "+code" },
-          ["<leader>gh"]     = { name = "+hunks" },
-          ["<leader>sn"]     = { name = "+noice" },
-          ["<leader>s"]      = { name = "+search" },
-          ["<localleader>u"] = { name = "+toggle" },
-          ["<leader>w"]      = { name = "+windows" },
-          ["gz"]             = { name = "+surround" },
-          ["<leader>d"]      = { name = "+debugger" },
-          ["<leader>f"]      = { name = "+file/find" },
-          ["<leader>u"]      = { name = "+ui/toggle" },
-          ["<leader>h"]      = { name = "+gitsigns/hop" },
-          ["<leader>q"]      = { name = "+quit/session" },
-          ["<leader>b"]      = { name = "+buffer/BufferLine" },
-          ["<leader>x"]      = { name = "+diagnostics/quickfix" },
-      }
+    whichkey.setup(opts)
+    local keymaps = {
+      mode               = { "n", "v" },
+      ["<leader>g"]      = { name = "+git" },
+      ["g"]              = { name = "+goto" },
+      ["]"]              = { name = "+next" },
+      ["["]              = { name = "+prev" },
+      ["<leader><tab>"]  = { name = "+tabs" },
+      ["<leader>c"]      = { name = "+code" },
+      ["<leader>gh"]     = { name = "+hunks" },
+      ["<leader>sn"]     = { name = "+noice" },
+      ["<leader>s"]      = { name = "+search" },
+      ["<localleader>u"] = { name = "+toggle" },
+      ["<leader>w"]      = { name = "+windows" },
+      ["gz"]             = { name = "+surround" },
+      ["<leader>d"]      = { name = "+debugger" },
+      ["<leader>f"]      = { name = "+file/find" },
+      ["<leader>u"]      = { name = "+ui/toggle" },
+      ["<leader>h"]      = { name = "+gitsigns/hop" },
+      ["<leader>q"]      = { name = "+quit/session" },
+      ["<leader>b"]      = { name = "+buffer/BufferLine" },
+      ["<leader>x"]      = { name = "+diagnostics/quickfix" },
+    }
 
-      wk.register(keymaps)
+    whichkey.register(keymaps)
   end,
 }
