6f92a886f068187c830eac904c1038153817a684
Nur Rony <pro.nmrony@gmail.com>
2024-02-06 21:00:25 +0600

    add bufferline and mini.bufremove plugin

---
+++ lua/nurrony/plugins/bufferline.lua
@@ -0,0 +1,76 @@
+-- This is what powers LazyVim's fancy-looking
+  -- tabs, which include filetype icons and close buttons.
+return {
+  "akinsho/bufferline.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' }, -- common functions library for other plugins
+    { "nvim-tree/nvim-web-devicons" }, -- add nerdfont devicons support
+  },
+  keys = {
+    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+    { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+    { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
+    { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+  },
+  opts = {
+    options = {
+      -- stylua: ignore
+      close_command = function(n) require("mini.bufremove").delete(n, false) end,
+      -- stylua: ignore
+      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+      left_mouse_command = "buffer %d",
+      buffer_close_icon = "󰅙",
+      modified_icon = "",
+      close_icon = "󰅙",
+      show_close_icon = false,
+      left_trunc_marker = "",
+      right_trunc_marker = "",
+      max_name_length = 14,
+      max_prefix_length = 13,
+      tab_size = 1,
+      show_tab_indicators = true,
+      indicator = {
+        style = "underline",
+      },
+      enforce_regular_tabs = false,
+      view = "multiwindow",
+      show_buffer_close_icons = true,
+      separator_style = "slant",
+      always_show_bufferline = true,
+      themable = true,
+      diagnostics = "nvim_lsp",
+      always_show_bufferline = false,
+      diagnostics_indicator = function(_, _, diag)
+        local icons = require("nurrony.core.defaults").icons.diagnostics
+        local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+          .. (diag.warning and icons.Warn .. diag.warning or "")
+        return vim.trim(ret)
+      end,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Neo-tree",
+          highlight = "Directory",
+          text_align = "left",
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("bufferline").setup(opts)
+    -- Fix bufferline when restoring a session
+    vim.api.nvim_create_autocmd("BufAdd", {
+      callback = function()
+        vim.schedule(function()
+          pcall(nvim_bufferline)
+        end)
+      end,
+    })
+  end,
+}
