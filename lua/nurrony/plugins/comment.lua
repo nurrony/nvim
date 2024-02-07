8aa98d043699cddf09bcf9b8e5b4b0756d3c6fd6
Nur Rony <pro.nmrony@gmail.com>
2024-02-08 03:00:48 +0600

    add comment plugin

--- lua/nurrony/plugins/comment.lua
+++ lua/nurrony/plugins/comment.lua
@@ -1,6 +1,9 @@
 return {
   "numToStr/Comment.nvim",
   event = { "BufNewFile", "BufReadPost" },
+  keys = {
+    { "gc", mode = { "n", "v", "i", "t" }, false },
+  },
   config = function(_, opts)
     require("Comment").setup(opts)
   end,
