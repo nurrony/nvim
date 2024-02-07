cc1e3c2e2d483830c229fd9caed3b662e97c6dcd
Nur Rony <pro.nmrony@gmail.com>
2024-02-08 02:01:04 +0600

    add mini comment

--- lua/nurrony/plugins/mini.lua
+++ lua/nurrony/plugins/mini.lua
@@ -1,29 +1,35 @@
 return {
-    -- buffer remove
-    {
-      "echasnovski/mini.bufremove",
-      event = { "BufReadPost", "BufNewFile" },
-      keys = {
-        {
-          "<leader>bd",
-          function()
-            local bd = require("mini.bufremove").delete
-            if vim.bo.modified then
-              local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
-              if choice == 1 then -- Yes
-                vim.cmd.write()
-                bd(0)
-              elseif choice == 2 then -- No
-                bd(0, true)
-              end
-            else
+  -- buffer remove
+  {
+    "echasnovski/mini.bufremove",
+    event = { "BufReadPost", "BufNewFile" },
+    keys = {
+      {
+        "<leader>bd",
+        function()
+          local bd = require("mini.bufremove").delete
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+            if choice == 1 then -- Yes
+              vim.cmd.write()
               bd(0)
+            elseif choice == 2 then -- No
+              bd(0, true)
             end
-          end,
-          desc = "Delete Buffer",
-        },
-        -- stylua: ignore
-        { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+          else
+            bd(0)
+          end
+        end,
+        desc = "Delete Buffer",
       },
+      -- stylua: ignore
+      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
     },
+  },
+  {
+    "echasnovski/mini.comment",
+    lazy = true,
+    config = true,
+    event = {"BufReadPost", "BufNewFile"},
+  },
 }
