37df9c595d701e7f6b0ace6d234a582517106b0d
Nur Rony <pro.nmrony@gmail.com>
2024-02-08 03:14:29 +0600

    add todo comment highlighting

--- lua/nurrony/plugins/comment.lua
+++ lua/nurrony/plugins/comment.lua
@@ -1,10 +1,29 @@
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufNewFile", "BufReadPost" },
-  keys = {
-    { "gc", mode = { "n", "v", "i", "t" }, false },
+  {
+    "numToStr/Comment.nvim",
+    event = { "BufNewFile", "BufReadPost" },
+    keys = {
+      { "gc", mode = { "n", "v", "i", "t" }, false },
+    },
+    config = function(_, opts)
+      require("Comment").setup(opts)
+    end,
   },
-  config = function(_, opts)
-    require("Comment").setup(opts)
-  end,
+  -- Finds and lists all of the TODO, HACK, BUG, etc comment
+  -- in your project and loads them into a browsable list.
+  {
+    "folke/todo-comments.nvim",
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    event = { "BufNewFile", "BufReadPost" },
+    config = true,
+    -- stylua: ignore
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+      { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    },
+  },
 }
