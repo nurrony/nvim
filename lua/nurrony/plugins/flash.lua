19d807d085d680a57e2ebfa062eea5962d7d4d41
Nur Rony <pro.nmrony@gmail.com>
2024-02-09 00:55:33 +0600

    add flash to navigate using buffer

---
+++ lua/nurrony/plugins/flash.lua
@@ -0,0 +1,17 @@
+  -- Flash enhances the built-in search functionality by showing labels
+  -- at the end of each match, letting you quickly jump to a specific
+  -- location.
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  vscode = true,
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+}
