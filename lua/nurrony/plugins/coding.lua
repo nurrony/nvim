2098b2cfcd13f512a80e07fcf0973dadcf01a787
Nur Rony <pro.nmrony@gmail.com>
2024-02-13 00:20:20 +0600

    update tsserver configuration

--- lua/nurrony/plugins/coding.lua
+++ lua/nurrony/plugins/coding.lua
@@ -46,7 +46,43 @@ return {
           },
         },
         dockerls = {},
-        tsserver = {},
+        ---@type lspconfig.options.tsserver
+        tsserver = {
+          keys = {
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.organizeImports.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Organize Imports",
+            },
+            {
+              "<leader>cR",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.removeUnused.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Remove Unused Imports",
+            },
+          },
+          ---@diagnostic disable-next-line: missing-fields
+          settings = {
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
         terraformls = {},
         bashls = { filetypes = { "bash", "sh" } },
         lua_ls = {
