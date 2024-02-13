534ec54e78d75efa005a4139528ed5a1d77dd718
Nur Rony <pro.nmrony@gmail.com>
2024-02-13 11:02:56 +0600

    update diagnostic sign register method

--- lua/nurrony/core/init.lua
+++ lua/nurrony/core/init.lua
@@ -12,20 +12,24 @@ vim.diagnostic.config(diagnostics_options)
 
 -- setup borders for handlers
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = diagnostics_options.float.border,
+  border = diagnostics_options.float.border,
 })
 --
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = diagnostics_options.float.border,
+  border = diagnostics_options.float.border,
 })
 
 -- configure diagnostics signs
 for name, icon in pairs(require("nurrony.core.defaults").icons.diagnostics) do
-    name = "DiagnosticSign" .. name
-    vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+  name = "DiagnosticSign" .. name
+  vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
 end
 
 -- configure debugger diagnostics signs
-for name, icon in pairs(require("nurrony.core.defaults").icons.debugger) do
-    vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+for name in pairs(require("nurrony.core.defaults").icons.debugger) do
+  sign = type(sign) == "table" and sign or { sign }
+  vim.fn.sign_define(
+    "Dap" .. name,
+    { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+  )
 end
