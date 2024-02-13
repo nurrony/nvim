78495a97fae434c2ac7dc07b04c1b162ea58c8cb
Nur Rony <pro.nmrony@gmail.com>
2024-02-13 21:27:22 +0600

    noice timeout set to 2sec

--- lua/nurrony/plugins/noice.lua
+++ lua/nurrony/plugins/noice.lua
@@ -16,7 +16,7 @@ return {
         },
       },
       opts = {
-        timeout = 3000,
+        timeout = 2000,
         max_height = function()
           return math.floor(vim.o.lines * 0.75)
         end,
