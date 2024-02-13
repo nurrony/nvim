53b85c54228ef27a14c762da46e335f703dc2c04
Nur Rony <pro.nmrony@gmail.com>
2024-02-14 00:50:14 +0600

    auto create directory if not exists

--- lua/nurrony/core/autocmds.lua
+++ lua/nurrony/core/autocmds.lua
@@ -98,6 +98,18 @@ autocmd("FileType", {
 --   end,
 -- })
 
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
 -- Remove Trailing whitespaces in all files
 autocmd({ "BufWritePre" }, { pattern = { "*" }, command = [[%s/\s\+$//e]] })
 
