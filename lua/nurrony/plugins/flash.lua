dbd8014d0757f5ecd4ee5d908316a7a12b7325ac
Nur Rony <pro.nmrony@gmail.com>
2024-02-13 21:43:47 +0600

    make ctrl key capital

--- lua/nurrony/plugins/flash.lua
+++ lua/nurrony/plugins/flash.lua
@@ -11,6 +11,6 @@ return {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }
