4ade5be4a750c1b82462c4c44e1b618dbeee0a3e
Nur Rony <pro.nmrony@gmail.com>
2024-02-13 21:19:12 +0600

    remove unnecessary key bindings

--- lua/nurrony/plugins/completion.lua
+++ lua/nurrony/plugins/completion.lua
@@ -82,11 +82,7 @@ return {
                   end,
               },
               mapping = cmp.mapping.preset.insert({
-                  ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                  ["<C-f>"] = cmp.mapping.scroll_docs(4),
                   ["<C-Space>"] = cmp.mapping.complete({ TriggerOnly = "triggerOnly" }),
-                  ["<C-n>"] = cmp.mapping.select_next_item(),
-                  ["<C-p>"] = cmp.mapping.select_prev_item(),
                   ["<C-e>"] = cmp.mapping({
                     i = cmp.mapping.abort(),
                     c = cmp.mapping.close(),
