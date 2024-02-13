81d14a93397748382724ba6be4d7b8e88306375a
Nur Rony <pro.nmrony@gmail.com>
2024-02-14 00:16:50 +0600

    cleanup unnecessary key binding

--- lua/nurrony/plugins/bufferline.lua
+++ lua/nurrony/plugins/bufferline.lua
@@ -13,8 +13,6 @@ return {
     { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
     { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
     { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
-    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
     { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
     { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
   },
