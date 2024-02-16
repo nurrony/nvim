62338f53917dfcd0a169d04683a988b9daac7281
Nur Rony <pro.nmrony@gmail.com>
2024-02-16 15:31:11 +0600

    correct formatting

--- lua/nurrony/plugins/toggleterm.lua
+++ lua/nurrony/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ return {
   keys = {
     { "<C-`>", "<cmd>Toggleterm<cr>", desc = "Open terminal" },
   },
-  config = function ()
+  config = function()
     require("toggleterm").setup {
       -- size can be a number or function which is passed the current terminal
       size = function(term)
@@ -24,7 +24,7 @@ return {
       -- on_exit = fun(t: Terminal, job: number, exit_code: number, name: string) -- function to run when terminal process exits
       hide_numbers = true, -- hide the number column in toggleterm buffers
       shade_filetypes = {},
-      autochdir = true, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+      autochdir = true,    -- when neovim changes it current directory the terminal will change it's own when next it's opened
       highlights = {
         NormalFloat = {
           link = 'TelescopeResultsNormal'
@@ -38,15 +38,15 @@ return {
       shade_terminals = false, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
       -- shading_factor = '<number>', -- the percentage by which to lighten terminal background, default: -30 (gets multiplied by -3 if background is light)
       start_in_insert = true,
-      insert_mappings = true, -- whether or not the open mapping applies in insert mode
+      insert_mappings = true,   -- whether or not the open mapping applies in insert mode
       terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
       persist_size = true,
-      persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+      persist_mode = true,      -- if set to true (default) the previous terminal mode will be remembered
       -- direction = 'vertical' | 'horizontal' | 'tab' | 'float',
       direction = 'float',
       close_on_exit = true, -- close the terminal window when the process exits
-      shell = vim.o.shell, -- change the default shell
-      auto_scroll = true, -- automatically scroll to the bottom on terminal output
+      shell = vim.o.shell,  -- change the default shell
+      auto_scroll = true,   -- automatically scroll to the bottom on terminal output
       -- This field is only relevant if direction is set to 'float'
       float_opts = {
         -- The border key is *almost* the same as 'nvim_open_win'
