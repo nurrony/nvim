a4b88092fc420a4561c8b1d63d07e7b9a4d5a3ab
Nur Rony <pro.nmrony@gmail.com>
2024-02-16 13:19:41 +0600

    fix formatting and comment formatting

--- lua/nurrony/plugins/dressing.lua
+++ lua/nurrony/plugins/dressing.lua
@@ -1,17 +1,18 @@
-  -- better vim.ui
+-- better vim.ui
 return {
   "stevearc/dressing.nvim",
+  event = "VeryLazy",
   lazy = true,
-  init = function()
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.ui.select = function(...)
-      require("lazy").load({ plugins = { "dressing.nvim" } })
-      return vim.ui.select(...)
-    end
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.ui.input = function(...)
-      require("lazy").load({ plugins = { "dressing.nvim" } })
-      return vim.ui.input(...)
-    end
-  end,
+  -- init = function()
+  --   ---@diagnostic disable-next-line: duplicate-set-field
+  --   vim.ui.select = function(...)
+  --     require("lazy").load({ plugins = { "dressing.nvim" } })
+  --     return vim.ui.select(...)
+  --   end
+  --   ---@diagnostic disable-next-line: duplicate-set-field
+  --   vim.ui.input = function(...)
+  --     require("lazy").load({ plugins = { "dressing.nvim" } })
+  --     return vim.ui.input(...)
+  --   end
+  -- end,
 }
