76265928189e5d467727dcc9d0230cd6e59fa085
Nur Rony <pro.nmrony@gmail.com>
2024-02-17 13:18:33 +0600

    remove commented line

--- lua/nurrony/plugins/flash.lua
+++ lua/nurrony/plugins/flash.lua
@@ -5,7 +5,6 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   opts = {},
-  -- stylua: ignore
   keys = {
     { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
     { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
