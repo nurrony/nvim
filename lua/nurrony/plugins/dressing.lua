39af71c6b36a73d17641239f25b758ea2affb1c0
Nur Rony <pro.nmrony@gmail.com>
2024-02-17 21:07:49 +0600

    update dressing configuration

--- lua/nurrony/plugins/dressing.lua
+++ lua/nurrony/plugins/dressing.lua
@@ -3,16 +3,16 @@ return {
   "stevearc/dressing.nvim",
   event = "VeryLazy",
   lazy = true,
-  -- init = function()
-  --   ---@diagnostic disable-next-line: duplicate-set-field
-  --   vim.ui.select = function(...)
-  --     require("lazy").load({ plugins = { "dressing.nvim" } })
-  --     return vim.ui.select(...)
-  --   end
-  --   ---@diagnostic disable-next-line: duplicate-set-field
-  --   vim.ui.input = function(...)
-  --     require("lazy").load({ plugins = { "dressing.nvim" } })
-  --     return vim.ui.input(...)
-  --   end
-  -- end,
+  init = function()
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.select = function(...)
+      require("lazy").load({ plugins = { "dressing.nvim" } })
+      return vim.ui.select(...)
+    end
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.input = function(...)
+      require("lazy").load({ plugins = { "dressing.nvim" } })
+      return vim.ui.input(...)
+    end
+  end,
 }
