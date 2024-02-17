e2a22a9ca04aea8b5f1dc6a0741ceba811d1a080
Nur Rony <pro.nmrony@gmail.com>
2024-02-17 16:47:21 +0600

    add mason and mason tool installer to automate language tools

--- lua/nurrony/plugins/coding.lua
+++ lua/nurrony/plugins/coding.lua
@@ -127,4 +127,65 @@ return {
       -- end
     end,
   },
+
+  -- install lsp tools and lsp servers
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      -- import mason
+      local mason = require("mason")
+
+      -- import mason-lspconfig
+      local mason_lspconfig = require("mason-lspconfig")
+
+      local mason_tool_installer = require("mason-tool-installer")
+
+      -- enable mason and configure icons
+      mason.setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+
+      mason_lspconfig.setup({
+        -- list of servers for mason to install
+        ensure_installed = {
+          "tsserver",
+          -- "html",
+          -- "cssls",
+          -- "tailwindcss",
+          -- "svelte",
+          "lua_ls",
+          -- "graphql",
+          -- "emmet_ls",
+          -- "prismals",
+          -- "pyright",
+        },
+        -- auto-install configured servers (with lspconfig)
+        automatic_installation = true, -- not the same as ensure_installed
+      })
+
+      mason_tool_installer.setup({
+        ensure_installed = {
+          "prettier", -- prettier formatter
+          "stylua",   -- lua formatter
+          -- "isort",    -- python formatter
+          -- "black",    -- python formatter
+          -- "pylint",   -- python linter
+          "eslint_d", -- js linter
+        },
+      })
+    end,
+  },
 }
