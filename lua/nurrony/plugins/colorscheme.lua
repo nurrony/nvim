d5d88cc78f6a73e4c0686f98883122b8cc4a6c1e
Nur Rony <pro.nmrony@gmail.com>
2024-02-19 20:22:38 +0600

    update colorscheme to catppiccin mocha

--- lua/nurrony/plugins/colorscheme.lua
+++ lua/nurrony/plugins/colorscheme.lua
@@ -80,7 +80,7 @@ return {
     },
     config = function(_, opts)
       require("catppuccin").setup(opts)
-      vim.cmd.colorscheme("catppuccin")
+      vim.cmd.colorscheme("catppuccin-mocha")
     end,
   },
 }
