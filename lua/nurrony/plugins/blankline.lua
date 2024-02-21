fe417e765dda881f8029dfb081e357bcf2923a37
Nur Rony <pro.nmrony@gmail.com>
2024-02-22 01:14:43 +0600

    update indent-blankline configuration

--- lua/nurrony/plugins/blankline.lua
+++ lua/nurrony/plugins/blankline.lua
@@ -1,13 +1,18 @@
 -- indent guides for Neovim
 return {
   "lukas-reineke/indent-blankline.nvim",
+  enabled = true,
   event = { "BufReadPost", "BufNewFile" },
   opts = {
     indent = {
       char = "│",
       tab_char = "│",
     },
-    scope = { enabled = false },
+    scope = {
+      enabled = false,
+      show_start = false,
+      show_end = false,
+    },
     exclude = {
       filetypes = {
         "help",
