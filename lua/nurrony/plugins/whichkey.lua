105e34537930e35fac2b06bf5dc89496b21048f4
Nur Rony <pro.nmrony@gmail.com>
2024-02-26 14:35:12 +0600

    add which key prefix for folds

--- lua/nurrony/plugins/whichkey.lua
+++ lua/nurrony/plugins/whichkey.lua
@@ -1,31 +1,31 @@
-    -- which-key
+-- which-key
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-      plugins = {
-        marks = true, -- shows a list of your marks on ' and `
-        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-        -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-        -- No actual key bindings are created
-        spelling = {
-          enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-          suggestions = 20, -- how many suggestions should be shown in the list?
-        },
-        presets = {
-          operators = true, -- adds help for operators like d, y, ...
-          motions = true, -- adds help for motions
-          text_objects = true, -- help for text objects triggered after entering an operator
-          windows = true, -- default bindings on <c-w>
-          nav = true, -- misc bindings to work with windows
-          z = true, -- bindings for folds, spelling and others prefixed with z
-          g = true, -- bindings for prefixed with g
-        },
+    plugins = {
+      marks = true,     -- shows a list of your marks on ' and `
+      registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+      -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+      -- No actual key bindings are created
+      spelling = {
+        enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+        suggestions = 20, -- how many suggestions should be shown in the list?
       },
-      -- add operators that will trigger motion and text object completion
-      -- to enable all native operators, set the preset / operators plugin above
-      operators = { gc = "Comments" },
-      motions = { count = true },
+      presets = {
+        operators = true,    -- adds help for operators like d, y, ...
+        motions = true,      -- adds help for motions
+        text_objects = true, -- help for text objects triggered after entering an operator
+        windows = true,      -- default bindings on <c-w>
+        nav = true,          -- misc bindings to work with windows
+        z = true,            -- bindings for folds, spelling and others prefixed with z
+        g = true,            -- bindings for prefixed with g
+      },
+    },
+    -- add operators that will trigger motion and text object completion
+    -- to enable all native operators, set the preset / operators plugin above
+    operators = { gc = "Comments" },
+    motions = { count = true },
   },
   config = function(_, opts)
     local whichkey = require("which-key")
@@ -39,6 +39,7 @@ return {
       ["["]              = { name = "+prev" },
       ["<leader><tab>"]  = { name = "+tabs" },
       ["<leader>c"]      = { name = "+code" },
+      ["z"]              = { name = "+folds" },
       ["<leader>gh"]     = { name = "+hunks" },
       ["<leader>sn"]     = { name = "+noice" },
       ["<leader>s"]      = { name = "+search" },
