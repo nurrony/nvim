af29016e87b21c4c2ed1e62fbb5af1115d41e22b
Nur Rony <pro.nmrony@gmail.com>
2024-02-26 17:59:10 +0600

    remove commented lines

--- lua/nurrony/plugins/dressing.lua
+++ lua/nurrony/plugins/dressing.lua
@@ -1,24 +1,5 @@
--- better vim.ui
--- return {
---   "stevearc/dressing.nvim",
---   event = "VeryLazy",
---   lazy = true,
---   -- init = function()
---   --   ---@diagnostic disable-next-line: duplicate-set-field
---   --   vim.ui.select = function(...)
---   --     require("lazy").load({ plugins = { "dressing.nvim" } })
---   --     return vim.ui.select(...)
---   --   end
---   --   ---@diagnostic disable-next-line: duplicate-set-field
---   --   vim.ui.input = function(...)
---   --     require("lazy").load({ plugins = { "dressing.nvim" } })
---   --     return vim.ui.input(...)
---   --   end
---   -- end,
-
--- }
-
-return { -- Better input/selection fields
+-- Better input/selection fields
+return {
   "stevearc/dressing.nvim",
   event = "VeryLazy",
   init = function()
