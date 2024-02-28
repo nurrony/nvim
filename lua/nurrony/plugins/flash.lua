e630a080879bd477b2d0ff952000f335fcc52a2e
Nur Rony <pro.nmrony@gmail.com>
2024-02-28 13:40:32 +0600

    update format file

--- lua/nurrony/plugins/flash.lua
+++ lua/nurrony/plugins/flash.lua
@@ -1,14 +1,41 @@
 -- Flash enhances the built-in search functionality by showing labels
--- at the end of each match, letting you quickly jump to a specific
--- location.
+-- at the end of each match, letting you quickly jump to a specific location.
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
   opts = {},
   keys = {
-    { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-    { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-    { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<C-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
+    },
+    {
+      "<C-s>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
+    },
   },
 }
