1e7cc205094072aefa930dbe0a0fe8f2db383a59
Nur Rony <pro.nmrony@gmail.com>
2024-03-04 16:29:22 +0600

    add FftT config for flash

--- lua/nurrony/plugins/flash.lua
+++ lua/nurrony/plugins/flash.lua
@@ -4,12 +4,33 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   opts = {
+    jump = { pos = "end", },
+    modes = {
+      char = {
+        -- highlight = {
+        --   matches = false,
+        -- },
+        -- autohide = true,
+        jump_labels = function(motion)
+          -- never show jump labels by default
+          -- return false
+          -- Always show jump labels for ftFT
+          return vim.v.count == 0 and motion:find("[ftFT]")
+          -- Show jump labels for ftFT in operator-pending mode
+          -- return vim.v.count == 0 and motion:find("[ftFT]") and vim.fn.mode(true):find("o")
+        end,
+      },
+    },
     prompt = {
       enabled = true,
       prefix = { { " 󰉂 ", "FlashPromptIcon" } },
     },
   },
   keys = {
+    { "f", mode = { "n", "x", "o" } },
+    { "F", mode = { "n", "x", "o" } },
+    { "t", mode = { "n", "x", "o" } },
+    { "T", mode = { "n", "x", "o" } },
     {
       "s",
       mode = { "n", "x", "o" },
