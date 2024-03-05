1ce3de64b34cbdeb41adff80ef667c33a5ec9de1
Nur Rony <pro.nmrony@gmail.com>
2024-03-05 16:37:28 +0600

    remove unnecessary require

--- lua/nurrony/plugins/bufferline.lua
+++ lua/nurrony/plugins/bufferline.lua
@@ -46,7 +46,7 @@ return {
       diagnostics = false, -- "nvim_lsp",
       always_show_bufferline = false,
       diagnostics_indicator = function(_, _, diag)
-        local icons = require("nurrony.core.configs").icons.diagnostics
+        local icons = Configs.icons.diagnostics
         local ret = (diag.error and icons.Error .. diag.error .. " " or "")
             .. (diag.warning and icons.Warn .. diag.warning or "")
         return vim.trim(ret)
