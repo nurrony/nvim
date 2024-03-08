59785f4727e35ead0bc03be9b869c1f816229d3e
Nur Rony <pro.nmrony@gmail.com>
2024-03-09 00:26:15 +0600

    update plugins

--- lua/nurrony/plugins/init.lua
+++ lua/nurrony/plugins/init.lua
@@ -1,5 +1,5 @@
 return {
-  { 'nvim-lua/plenary.nvim', lazy = true }, -- common functions library for other plugins
-  { "MunifTanjim/nui.nvim", lazy = true }, -- better vim.notify
+  { 'nvim-lua/plenary.nvim',       lazy = true }, -- common functions library for other plugins
+  { "MunifTanjim/nui.nvim",        lazy = true }, -- better vim.notify
   { "nvim-tree/nvim-web-devicons", lazy = true }, -- add nerdfont devicons support
 }
