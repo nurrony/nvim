d40f0fc610dc4839b8e8216b415c43c685313daa
Nur Rony <pro.nmrony@gmail.com>
2024-03-13 16:24:57 +0600

    move java lsp configuration to its file

--- lua/nurrony/plugins/adapters/java.lua
+++ lua/nurrony/plugins/adapters/java.lua
@@ -19,6 +19,22 @@ end
 
 return {
   -- Set up nvim-jdtls to attach to java files.
+  -- Configure nvim-lspconfig to install the server automatically via mason, but
+  -- defer actually starting it to our configuration of nvim-jtdls below.
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        jdtls = {},
+      },
+      setup = {
+        jdtls = function()
+          return true -- avoid duplicate servers
+        end,
+      },
+    },
+  },
   {
     "mfussenegger/nvim-jdtls",
     dependencies = { "folke/which-key.nvim" },
