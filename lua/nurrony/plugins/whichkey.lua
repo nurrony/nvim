272755db25867b0d71bc1f9758056c58207831d1
Nur Rony <pro.nmrony@gmail.com>
2024-03-17 16:50:07 +0600

    update which key

--- lua/nurrony/plugins/whichkey.lua
+++ lua/nurrony/plugins/whichkey.lua
@@ -9,27 +9,27 @@ return {
       group = "",
     },
     layout = {
-      spacing = 5,
+      spacing = 10,
       align = "left",
     },
     show_help = false,
     plugins = {
-      marks = true, -- shows a list of your marks on ' and `
+      marks = true,     -- shows a list of your marks on ' and `
       registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
       -- the presets plugin, adds help for a bunch of default keybindings in Neovim
       -- No actual key bindings are created
       spelling = {
-        enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+        enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
         suggestions = 20, -- how many suggestions should be shown in the list?
       },
       presets = {
-        operators = true, -- adds help for operators like d, y, ...
-        motions = true, -- adds help for motions
+        operators = true,    -- adds help for operators like d, y, ...
+        motions = true,      -- adds help for motions
         text_objects = true, -- help for text objects triggered after entering an operator
-        windows = true, -- default bindings on <c-w>
-        nav = true, -- misc bindings to work with windows
-        z = true, -- bindings for folds, spelling and others prefixed with z
-        g = true, -- bindings for prefixed with g
+        windows = true,      -- default bindings on <c-w>
+        nav = true,          -- misc bindings to work with windows
+        z = true,            -- bindings for folds, spelling and others prefixed with z
+        g = true,            -- bindings for prefixed with g
       },
     },
     -- add operators that will trigger motion and text object completion
