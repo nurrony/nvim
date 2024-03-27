6f35af7899ba86d7a21e1cf297df39f848a3629d
Nur Rony <pro.nmrony@gmail.com>
2024-03-28 05:55:24 +0600

    update javascript parser in typescript

--- lua/nurrony/plugins/languages/typescript.lua
+++ lua/nurrony/plugins/languages/typescript.lua
@@ -7,7 +7,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
+        vim.list_extend(opts.ensure_installed, { "typescript", "tsx", "javascript" })
       end
     end,
   },
