f149dd5171c975c49678b2318410fbd11c26e137
Nur Rony <pro.nmrony@gmail.com>
2024-03-28 05:18:55 +0600

    ensure java dap protocol

--- lua/nurrony/plugins/languages/java.lua
+++ lua/nurrony/plugins/languages/java.lua
@@ -28,6 +28,21 @@ return {
     end,
   },
 
+  -- Ensure java debugger and test packages are installed.
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "java-test", "java-debug-adapter" })
+        end,
+      },
+    },
+  },
+
   -- correctly setup lspconfig
   {
     "neovim/nvim-lspconfig",
