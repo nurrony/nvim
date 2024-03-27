0dabd9baffc2fc7303227c14abbc505bb68f6c46
Nur Rony <pro.nmrony@gmail.com>
2024-03-28 05:18:01 +0600

    add treesitter parser for java

--- lua/nurrony/plugins/languages/java.lua
+++ lua/nurrony/plugins/languages/java.lua
@@ -19,6 +19,15 @@ end
 
 return {
 
+  -- Add java to treesitter.
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "java" })
+    end,
+  },
+
   -- correctly setup lspconfig
   {
     "neovim/nvim-lspconfig",
