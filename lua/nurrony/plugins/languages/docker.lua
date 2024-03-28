64ca502c8ff8f8c38d625db4e8a06dbcdd1523ab
Nur Rony <pro.nmrony@gmail.com>
2024-03-28 11:30:38 +0600

    fix language server installation for docker

--- lua/nurrony/plugins/languages/docker.lua
+++ lua/nurrony/plugins/languages/docker.lua
@@ -24,11 +24,20 @@ return {
           vim.list_extend(opts.ensure_installed, { "hadolint" })
         end,
       },
-    }
+    },
   },
 
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "dockerfile-language-server", "docker-compose-language-service" })
+        end,
+      },
+    },
     opts = {
       servers = {
         dockerls = {},
