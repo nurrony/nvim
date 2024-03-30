c87b9f3aacf9598b691c3f529f866ccd3a4042d0
Nur Rony <pro.nmrony@gmail.com>
2024-03-31 01:46:01 +0600

    update typescript configuration

--- lua/nurrony/plugins/languages/typescript.lua
+++ lua/nurrony/plugins/languages/typescript.lua
@@ -32,6 +32,29 @@ return {
             completions = {
               completeFunctionCalls = true,
             },
+            -- specify some or all of the following settings if you want to adjust the default behavior
+            javascript = {
+              inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+              },
+            },
+            typescript = {
+              inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+              },
+            },
           },
         },
       },
